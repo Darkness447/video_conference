7a8e02d0651f9c188593b3d5f151d2ea63b8f883
Darkness <agrawaldeepesh546@gmail.com>
2024-04-05 17:32:56 +0530

    Refactor chat client and hub code, and add webrtc package and room connection logic

--- pkg/chat/client.go
+++ pkg/chat/client.go
@@ -1,6 +1,8 @@
 package chat
 
 import (
+	"bytes"
+	"log"
 	"time"
 
 	"github.com/fasthttp/websocket"
@@ -30,11 +32,75 @@ type Client struct {
 }
 
 func (c *Client) readPump() {
+	defer func() {
+		c.Hub.unregister <- c
+		c.Conn.Close()
+	}()
 
+	c.Conn.SetReadLimit(maxMessage)
+	c.Conn.SetReadDeadline(time.Now().Add(pongWait))
+	c.Conn.SetPongHandler(func(string) error {
+		c.Conn.SetReadDeadline(time.Now().Add(pongWait))
+		return nil
+	})
+
+	for {
+		_, message, err := c.Conn.ReadMessage()
+
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				log.Printf("error: %v", err)
+			}
+
+			break
+		}
+
+		message = bytes.TrimSpace(bytes.Replace(message, newLine, space, -1))
+		c.Hub.broadcast <- message
+
+	}
 }
 
 func (c *Client) writePump() {
+	ticker := time.NewTicker(pingPeriod)
+	defer func() {
+		ticker.Stop()
+		c.Conn.Close()
+	}()
 
+	for {
+		select {
+		case message, ok := <-c.Send:
+			c.Conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if !ok {
+				return
+			}
+
+			w, err := c.Conn.NextWriter(websocket.TextMessage)
+			if err != nil {
+				return
+			}
+
+			w.Write(message)
+
+			n := len(c.Send)
+
+			for i := 0; i < n; i++ {
+				w.Write(newLine)
+				w.Write(<-c.Send)
+			}
+
+			if err := w.Close(); err != nil {
+				return
+			}
+
+		case <-ticker.C:
+			c.Conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if err := c.Conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+				return
+			}
+		}
+	}
 }
 
 func PeerChatConn(c *websocket.Conn, hub *Hub) {
